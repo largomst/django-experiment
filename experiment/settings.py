d0717dc9fe0f6f9c89cd3b5467582c5b33d58eb5
largomst <dingrui.zhen@gmail.com>
2020-03-26 17:45:02 +0800

    修复拼写错误

--- experiment/settings.py
+++ experiment/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_FILE = {}
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
