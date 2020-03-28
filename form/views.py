361b197482ec4219eedf79cbef661e66e11591f5
largomst <dingrui.zhen@gmail.com>
2020-03-28 23:01:02 +0800

    最原始的表单使用方法

--- form/views.py
+++ form/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 
 
 def index(request):
+    if request.method == 'POST':
+        property = request.POST.get('property')
+        print(property)
+        redirect('index')
+    else:
+        pass
+
     template_name = "index.html"
     return render(request, template_name)
